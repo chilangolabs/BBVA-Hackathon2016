1902bc071bfd19ad96acff1232898bb8a72a8db9
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-10-30 11:32:35 -0600

    agregando detail activity

--- app/src/main/java/com/chilangolabs/capitallab/ProfileActivity.kt
+++ app/src/main/java/com/chilangolabs/capitallab/ProfileActivity.kt
@@ -2,10 +2,12 @@ package com.chilangolabs.capitallab
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.content_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -15,6 +17,14 @@ class ProfileActivity : AppCompatActivity() {
         toolbar.title = ""
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        btnCafeteria.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+//            val pair1 = Pair(imgCafeteria, "imgdetail")
+//            val pair2 = Pair(txtCafeteria, "nameDetail")
+            val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, imgCafeteria, "imgdetail")
+            startActivity(intent, options.toBundle())
+        }
 
     }
 
